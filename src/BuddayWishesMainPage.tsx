5efb14ee5d06b280b681761fc60cfb5943a26424
ns056642 <Ninad.Shenoy@cerner.com>
2022-05-28 17:59:36 +0530

    Updating it to my birthday

--- src/BuddayWishesMainPage.tsx
+++ src/BuddayWishesMainPage.tsx
@@ -2,10 +2,9 @@ import React, { useState, useEffect } from 'react';
 import './App.css';
 import useWindowSize from './hooks/useWindowSize';
 import Confetti from 'react-confetti';
-import image from './image-meenakshi.jpeg';
 
 function BuddayWishesMainPage() {
-  const name = "Meenu";
+  const name = "Ninad";
   const { width, height } = useWindowSize();
   const [visible, setVisible] = useState(true);
 
@@ -14,7 +13,7 @@ function BuddayWishesMainPage() {
     // Update the count down every 1 second
     var x = setInterval(function () {
 
-      var countDownDate = new Date("Apr 27, 2022 00:00:0").getTime();
+      var countDownDate = new Date("Jul 14, 2022 00:00:0").getTime();
       // Get today's date and time
       var now = new Date().getTime();
 
@@ -43,8 +42,8 @@ function BuddayWishesMainPage() {
   const text = visible ? (
     <div className="countdown-div" >
       <h1 className="countdown-text" > Countdown to {name}'s budday</h1>
+      <h4>Wishing myself, because nobody else will</h4>
       <h1 className="countdown-timer" id="countdown-timer" > Wait for it </h1>
-      <h3>#CSKColours</h3>
     </div>) :
     (<div className="wish-div">
       <Confetti
@@ -52,12 +51,7 @@ function BuddayWishesMainPage() {
         height={height}
       />
       <h1 className="wish-text"> Happy budday {name}!</h1>
-      <div className='image-div'>
-        <img className='image-element' alt="birthday girl ugly" src={image} />
-      </div>
-      <h1>IT IS YOUR BIRTHDAY!!</h1>
-      <h3>And, you haven't aged at all, congratulations!</h3>
-      <h3>#forever21</h3>
+      <h1>IT IS MY BIRTHDAY!!</h1>
     </div>)
 
   return (
